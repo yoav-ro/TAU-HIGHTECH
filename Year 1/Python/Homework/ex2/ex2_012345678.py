c9ccf098d87d0e7dab42afa7a3991d244fe573a5
yoav-ro <yoavro.pro@gmail.com>
2024-01-15 15:05:58 +0200

    task 2

--- Year 1/Python/Homework/ex2/ex2_012345678.py
+++ Year 1/Python/Homework/ex2/ex2_012345678.py
@@ -4,24 +4,24 @@
 #########################################
 # Question 1 - do not delete this comment
 #########################################
-str1 = "abcfefgcba"  # Replace ??? with a string
-res = True
-if len(str1) % 2 != 0:
-    res = False
-firstThree = str1[0:3]
-if (
-    str1[0] != str1[len(str1) - 1]
-    and str1[1] != str1[len(str1) - 2]
-    and str1[2] != str1[len(str1) - 3]
-):
-    res = False
-if str1[3] != "F" and str1[3] != "f":
-    res = False
+# str1 = "abcfefgcba"  # Replace ??? with a string
+# res = True
+# if len(str1) % 2 != 0:
+#     res = False
+# firstThree = str1[0:3]
+# if (
+#     str1[0] != str1[len(str1) - 1]
+#     and str1[1] != str1[len(str1) - 2]
+#     and str1[2] != str1[len(str1) - 3]
+# ):
+#     res = False
+# if str1[3] != "F" and str1[3] != "f":
+#     res = False
 
-if res:
-    print("Yes")
-else:
-    print("No")
+# if res:
+#     print("Yes")
+# else:
+#     print("No")
 
 # Write the rest of the code for question 1 below here.
 
@@ -29,11 +29,16 @@ else:
 #########################################
 # Question 2 - do not delete this comment
 #########################################
-# A = ??? # Replace ??? with a list of numbers (int/float).
-# a = ??? # Replace ??? with a positive int.
-# b = ??? # Replace ??? with a positive int.
+A = [4, 11, 12, 18, 22, 24]  # Replace ??? with a list of numbers (int/float).
+a = 3  # Replace ??? with a positive int.
+b = 10  # Replace ??? with a positive int.
 # Write the rest of the code for question 2 below here.
-
+ret = -1
+for idx, num in enumerate(A):
+    if num % a == 0 and num % b == 0:
+        ret = idx
+        break
+print(ret)
 
 #########################################
 # Question 3 - do not delete this comment
