b704d7d3382bb9528c20f3650272d07789212144
yoav-ro <yoavro.pro@gmail.com>
2024-01-15 14:38:48 +0200

    ex2
    תרגיל 1 וסידור חדש של התיקיות

---
+++ Year 1/Python/Homework/ex2/ex2_012345678.py
@@ -0,0 +1,62 @@
+# Python Programming - Ex2
+
+
+#########################################
+# Question 1 - do not delete this comment
+#########################################
+str1 = "abcfefgcba"  # Replace ??? with a string
+res = True
+if len(str1) % 2 != 0:
+    res = False
+firstThree = str1[0:3]
+if (
+    str1[0] != str1[len(str1) - 1]
+    and str1[1] != str1[len(str1) - 2]
+    and str1[2] != str1[len(str1) - 3]
+):
+    res = False
+if str1[3] != "F" and str1[3] != "f":
+    res = False
+
+if res:
+    print("Yes")
+else:
+    print("No")
+
+# Write the rest of the code for question 1 below here.
+
+
+#########################################
+# Question 2 - do not delete this comment
+#########################################
+# A = ??? # Replace ??? with a list of numbers (int/float).
+# a = ??? # Replace ??? with a positive int.
+# b = ??? # Replace ??? with a positive int.
+# Write the rest of the code for question 2 below here.
+
+
+#########################################
+# Question 3 - do not delete this comment
+#########################################
+# C = ??? # Replace ??? with a list of numbers (int/float).
+
+# Write the rest of the code for question 3 below here.
+
+
+#########################################
+# Question 4 - do not delete this comment
+#########################################
+# D = ??? # Replace ??? with a list of numbers (int/float).
+# E = ??? # Replace ??? with a list of numbers (int/float).
+
+
+# Write the rest of the code for question 4 below here.
+
+
+#########################################
+# Question 5 - do not delete this comment
+#########################################
+# my_string = ??? # Replace ??? with a string
+# k = ??? # Replace ??? with a positive int.
+
+# Write the code for question 5 below here.
