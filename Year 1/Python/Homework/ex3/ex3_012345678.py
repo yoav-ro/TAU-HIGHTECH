e363da34a8b36abc017ce7e8a52c4f58b0880083
yoav-ro <yoavro.pro@gmail.com>
2024-02-05 14:11:33 +0200

    ex3 initial

---
+++ Year 1/Python/Homework/ex3/ex3_012345678.py
@@ -0,0 +1,45 @@
+# Python Programming - Ex3
+
+#########################################
+# Question 1 - do not delete this comment
+#########################################
+def is_in_range(lst1,a ,b):
+	# Write the rest of the code for question 1 below here.
+
+
+#########################################
+# Question 2 - do not delete this comment
+#########################################
+# def first_upper_str(lst2):
+	# Write the rest of the code for question 2 below here.
+
+
+#########################################
+# Question 3 - do not delete this comment
+#########################################
+# def drop_duplicates(lst3):
+	# Write the rest of the code for question 3a below here.
+# def drop_duplicates_in_place(lst3):
+	# Write the rest of the code for question 3b below here.
+
+
+#########################################
+# Question 4 - do not delete this comment
+#########################################
+# def is_contain(lst4, lst5):
+	# Write the rest of the code for question 4 below here.
+
+
+#########################################
+# Question 5 - do not delete this comment
+#########################################
+# def mul_elementwise(mat1, mat2):
+	# Write the rest of the code for question 5 below here.
+
+
+#########################################
+# Question 6 - do not delete this comment
+#########################################
+# def mat_mul_vec(mat3, vec):
+	# Write the rest of the code for question 6 below here.
+
---
+++ Year 1/Python/Lectures/tests.py
@@ -0,0 +1,7 @@
+def fortest(num):
+    for i in range(0, num + 1):
+        if i >= 0:
+            print(i)
+            i -= 1
+
+    test = {"Yoav": "Ronen"}
