ebba322779693b17852c57925684cf6203151083
yoav-ro <yoavro.pro@gmail.com>
2024-02-12 14:19:29 +0200

    task 1

--- Year 1/Python/Homework/ex4/ex4_012345678.py
+++ Year 1/Python/Homework/ex4/ex4_012345678.py
@@ -1,30 +1,32 @@
 # Python Programming - Ex4
 
+
 #########################################
 # Question 1 - do not delete this comment
 #########################################
 def lists_to_simple_dict(names, dates):
-	 # Write the rest of the code for question 1 below here.
-    
-	 
+    # Write the rest of the code for question 1 below here.
+    covidTest = {}
+    for i, name in enumerate(names):
+        covidTest[name] = dates[i]
+    return covidTest
 
-#########################################
+# #########################################
 # Question 2 - do not delete this comment
 #########################################
 # def lists_to_complex_dict(names, dates):
-	# Write the rest of the code for question 2 below here.
+# Write the rest of the code for question 2 below here.
 
 
 #########################################
 # Question 3 - do not delete this comment
 #########################################
 # def find_substring_location(s, k):
-	# Write the rest of the code for question 3a below here.
+# Write the rest of the code for question 3a below here.
 
 
 #########################################
 # Question 4 - do not delete this comment
 #########################################de
-# def mul_sparse_matrices(d1, d2): 
-	# Write the rest of the code for question 4 below here.
-
+# def mul_sparse_matrices(d1, d2):
+# Write the rest of the code for question 4 below here.
