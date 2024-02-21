614c30c07bbcfec23083222bc29ffc917da38df5
yoav-ro <yoavro.pro@gmail.com>
2024-02-21 13:41:42 +0200

    task 1 no input validation

--- Year 1/Python/Homework/ex5/ex5_012345678.py
+++ Year 1/Python/Homework/ex5/ex5_012345678.py
@@ -1,37 +1,45 @@
-#Exercise 5: Python Programming 
+# Exercise 5: Python Programming
+
 
 #########################################
 # Question 1 - do not delete this comment
 #########################################
 def mean_nums(file):
-# Write the rest of the code for question 1 below here.
+    # Write the rest of the code for question 1 below here.
+    path = "Year 1\Python\Homework\ex5"
+    f = open(path + "/" + file, "r")
+    numList = f.read().split(" ")
+    return sum([(int)(num) for num in numList]) / len(numList)
+
+
+print(mean_nums("q1.txt"))
 
 #########################################
 # Question 2 - do not delete this comment
 #########################################
-def copy_long_words(infile, outfile, k):
+# def copy_long_words(infile, outfile, k):
 # Write the rest of the code for question 2 below here.
-	
+
 #########################################
 # Question 3 - do not delete this comment
 #########################################
-def get_x_freqs(infile, outfile, x):
+# def get_x_freqs(infile, outfile, x):
 # Write the rest of the code for question 3 below here.
 
 #########################################
 # Question 4 - do not delete this comment
 #########################################
-def decode(in_file, out_file):
+# def decode(in_file, out_file):
 # Write the rest of the code for question 4 below here.
 
 #########################################
 # Question 5 - do not delete this comment
 #########################################
-def process_contacts(contacts_file):
+# def process_contacts(contacts_file):
 # Write the rest of the code for question 5 below here.
 
 #########################################
 # Question 6 - do not delete this comment
 #########################################
-def get_covid_cases_by_date(filename, date):
+# def get_covid_cases_by_date(filename, date):
 # Write the rest of the code for question 6 below here.
