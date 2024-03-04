da8d9967583a162ce409e8abfb40fc03d2a111bc
yoav-ro <yoavro.pro@gmail.com>
2024-03-04 14:53:45 +0200

    task 1

--- Year 1/Python/Homework/ex6/ex6_322317348.py
+++ Year 1/Python/Homework/ex6/ex6_322317348.py
@@ -1,42 +1,44 @@
-#Exercise 6: Python Programming 
+# Exercise 6: Python Programming
+
 
 #########################################
 # Question 1 - do not delete this comment
 #########################################
 def reverse_string(s):
     # Write the rest of the code for question 1 below here.
-	
+    if len(s) == 1 or len(s) == 0:
+        return s
+    return s[len(s) - 1] + reverse_string(s[: len(s) - 1])
+
+
 #########################################
 # Question 2 - do not delete this comment
 #########################################
-def max_rec(lst):
-    # Write the rest of the code for question 2 below here.
+# def max_rec(lst):
+# Write the rest of the code for question 2 below here.
 
-	
+
 #########################################
 # Question 3 - do not delete this comment
 #########################################
-def can_return_to_earth(weights, W, K)
-    # Write the rest of the code for question 3 below here.
+# def can_return_to_earth(weights, W, K)
+# Write the rest of the code for question 3 below here.
 
 #########################################
 # Question 4 - do not delete this comment
-#########################################	
-def is_changeable(n, lst):
-    # Write the rest of the code for question 4 below here.
+#########################################
+# def is_changeable(n, lst):
+# Write the rest of the code for question 4 below here.
 
 
 #########################################
 # Question 5a - do not delete this comment
-#########################################	
-def climb_combinations(n):
+#########################################
+# def climb_combinations(n):
 # Write the rest of the code for question 5a below here.
 
 #########################################
 # Question 5b - do not delete this comment
 #########################################
-def climb_combinations_memo(n, memo=None):
+# def climb_combinations_memo(n, memo=None):
 # Write the rest of the code for question 5b below here.
-
-
-
