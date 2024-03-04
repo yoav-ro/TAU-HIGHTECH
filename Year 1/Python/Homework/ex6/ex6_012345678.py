97d6bf4f75d765b824a53a5e1c44d73abd1de7b3
yoav-ro <yoavro.pro@gmail.com>
2024-03-04 14:38:21 +0200

    initial commit ex6

---
+++ Year 1/Python/Homework/ex6/ex6_012345678.py
@@ -0,0 +1,42 @@
+#Exercise 6: Python Programming 
+
+#########################################
+# Question 1 - do not delete this comment
+#########################################
+def reverse_string(s):
+    # Write the rest of the code for question 1 below here.
+	
+#########################################
+# Question 2 - do not delete this comment
+#########################################
+def max_rec(lst):
+    # Write the rest of the code for question 2 below here.
+
+	
+#########################################
+# Question 3 - do not delete this comment
+#########################################
+def can_return_to_earth(weights, W, K)
+    # Write the rest of the code for question 3 below here.
+
+#########################################
+# Question 4 - do not delete this comment
+#########################################	
+def is_changeable(n, lst):
+    # Write the rest of the code for question 4 below here.
+
+
+#########################################
+# Question 5a - do not delete this comment
+#########################################	
+def climb_combinations(n):
+# Write the rest of the code for question 5a below here.
+
+#########################################
+# Question 5b - do not delete this comment
+#########################################
+def climb_combinations_memo(n, memo=None):
+# Write the rest of the code for question 5b below here.
+
+
+
