bfb3f430d02efd9e95f149f9bf1904423ccebd71
yoav-ro <89576513+yoav-ro@users.noreply.github.com>
2024-03-28 21:04:51 +0200

    ex7 initial

---
+++ Year 1/Python/Homework/ex7/ex7_012345678.py
@@ -0,0 +1,79 @@
+''' Exercise #7. Python Programming.'''
+#########################################
+# Question 1 - do not delete this comment
+#########################################
+
+class ArtDisplay:
+
+    def __init__(self, name, date, art_type, preserving_date, worth):
+
+    def change_preserving_date(self,new_date):
+
+
+#########################################
+# Question 2 - do not delete this comment
+#########################################
+class MuseumSubscriber:
+
+    def __init__(self, name, ticket_type, favorites):
+
+    def set_entry(self):
+
+    def get_favorites(self):
+
+
+##########################################
+# Questions 3 - do not delete this comment
+##########################################
+
+class Museum:
+
+    def __init__(self,art_displays):
+
+    def get_art_displays(self):
+
+    def get_art_display(self,name):
+
+    def add_art_display(self,artDisplay):
+
+    def add_subscriber(self, subscriber):
+
+    def change_preserving_date(self, name , new_date):
+
+    def get_total_worth(self):
+
+    def subscriber_entry(self,name):
+
+    def find_loved_disp(self):
+
+#########################################
+# Question 4 - do not delete this comment
+#########################################
+
+def create_museum(filename):
+ 
+
+
+
+
+# use the following code to test your code:
+#museum=create_museum('museum.csv')
+#print (museum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
